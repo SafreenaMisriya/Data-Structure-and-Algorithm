91c2efa7f05a96e4349ec2307f54e6f7857c4ce3
Safreena Misriya R <safreenamisriya02@gmail.com>
2024-02-18 12:33:08 +0530

    code

--- DSA_1/binary_tree.dart/binary.dart
+++ DSA_1/binary_tree.dart/binary.dart
@@ -1,170 +1,63 @@
-class Node {
-  int data;
-  Node? left, right;
-  
-  Node(this.data);
+class TreeNode {
+  late int data;
+  late TreeNode left;
+  late TreeNode right;
+
+  TreeNode(this.data);
 }
 
-class BinarySearchTree {
-  Node? root;
-  
-  void insert(int data) {
-    Node? currentnode = root;
-    if (root == null) {
-      root = Node(data);
-      return;
-    }
-    while (true) {
-      if (data < currentnode!.data) {
-        if (currentnode.left == null) {
-          currentnode.left = Node(data);
-          break;
-        } else {
-          currentnode = currentnode.left;
-        }
-      } else {
-         if (currentnode.right == null) {
-          currentnode.right = Node(data);
-          break;
-        } else {
-          currentnode = currentnode.right;
-        }
-      }
-    }
+class BinaryTree {
+  late TreeNode root;
+
+  BinaryTree() {
+    root = TreeNode(0); // Initialize root with a dummy value
   }
 
-  bool contains(int data) {
-    Node? currentnode = root;
-    while (currentnode != null) {
-      if (data < currentnode.data) {
-        currentnode = currentnode.left;
-      } else if (data > currentnode.data) {
-        currentnode = currentnode.right;
-      } else {
-        return true;
-      }
+  // Function to insert a new node into the binary tree
+  void insert(int value) {
+    root = _insertRecursive(root, value);
+  }
+
+  TreeNode _insertRecursive(TreeNode node, int value) {
+    if (node.data == 0) {
+      // If the node is a dummy node, create a new node with the given value
+      return TreeNode(value);
     }
-    return false;
-  }
 
-  void remove(int data) {
-    removeHelper(data, root, null);
-  }
-
-  void removeHelper(int data, Node? currentnode, Node? parentNode) {
-    while (currentnode != null) {
-      if (data < currentnode.data) {
-        parentNode = currentnode;
-        currentnode = currentnode.left;
-      } else if (data > currentnode.data) {
-        parentNode = currentnode;
-        currentnode = currentnode.right;
-      } else {
-        if (currentnode.left != null && currentnode.right != null) {
-          currentnode.data = getMinValue(currentnode.right);
-          removeHelper(currentnode.data, currentnode.right, currentnode);
-        } else {
-          if (parentNode == null) {
-            if (currentnode.right == null) {
-              root = currentnode.left;
-            } else {
-              root = currentnode.right;
-            }
-          } else {
-            if (parentNode.left == currentnode) {
-              if (currentnode.right == null) {
-                parentNode.left = currentnode.left;
-              } else {
-                parentNode.left = currentnode.right;
-              }
-            } else {
-              if (currentnode.right == null) {
-                parentNode.right = currentnode.left;
-              } else {
-                parentNode.right = currentnode.right;
-              }
-            }
-          }
-          break;   
-        }
-      }
+    if (value < node.data) {
+      // If the value is less than the current node's data, insert into the left subtree
+      node.left = _insertRecursive(node.left, value);
+    } else if (value > node.data) {
+      // If the value is greater than the current node's data, insert into the right subtree
+      node.right = _insertRecursive(node.right, value);
     }
+
+    return node;
   }
 
-  int getMinValue(Node? currentnode) {
-    if(currentnode!.left==null){
-      return currentnode.data;
-    }
-    else{
-      return getMinValue(currentnode.left);
-    }
-  }
-
-  void inorder() {
-    inorderHelper(root);
-  }
-
-  void inorderHelper(Node? node) {
-    if (node != null) {
-      inorderHelper(node.left);
+  // Function to perform an inorder traversal of the binary tree
+  void inorderTraversal(TreeNode node) {
+    if (node.data != 0) {
+      inorderTraversal(node.left);
       print(node.data);
-      inorderHelper(node.right);
+      inorderTraversal(node.right);
     }
-  }
-  void preorder(){
-   preorderHelper(root);
-  }
-  preorderHelper(Node? node){
-   if(node!=null){
-    print(node.data);
-    preorderHelper(node.left);
-    preorderHelper(node.right);
-   }
-  }
-  postorder(){
-    postorderHelper(root);
-  }
-  postorderHelper(Node? node){
-   if(node!=null){
-    postorderHelper(node.left);
-    postorderHelper(node.right);
-    print(node.data);
-   }
-  }
-
-  int findClosest(int target) {
-    Node? current = root;
-    int closest = current!.data;
-    while (current != null) {
-      if ((target - closest).abs() > (target - current.data).abs()) {
-        closest = current.data;
-      }
-      if (current.data < target) {
-        current = current.right;
-      } else if (current.data > target) {
-        current = current.left;
-      } else {
-        break;
-      }
-    }
-    return closest;
   }
 }
 
 void main() {
-  BinarySearchTree tree = BinarySearchTree();
-  tree.insert(12);
-  tree.insert(8);
-  tree.insert(3);
-  tree.insert(2);
-  // tree.remove(3);
-  print(tree.contains(3));
-  print('*****');
-  print(tree.findClosest(7));
-  print('*****');
-  tree.inorder();
-  print('ssfsadfdfadf');
-  tree.preorder();
-  print('fsadfdjfadhfk');
-  tree.postorder();
+  BinaryTree binaryTree = BinaryTree();
+
+  // Insert some values into the binary tree
+  binaryTree.insert(5);
+  binaryTree.insert(3);
+  binaryTree.insert(7);
+  binaryTree.insert(2);
+  binaryTree.insert(4);
+  binaryTree.insert(6);
+  binaryTree.insert(8);
+
+  // Perform an inorder traversal and print the values
+  print("Inorder Traversal:");
+  binaryTree.inorderTraversal(binaryTree.root);
 }
